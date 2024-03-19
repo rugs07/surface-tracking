0ff7110b7fbb9d5ff11aa887e63c4a2e020d83dd
91639 <goel.aryan2406@gmail.com>
2024-03-19 17:13:59 +0530

    Revert "[add]VR.js"
    This reverts commit c07d082b42ccd5a8ba6a73910e9a85e731b5507b.

--- src/source/App.jsx
+++ src/source/App.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
 import Home from './Components/Home'
-import VR from './Components/VR'
 
 const App = () => {
   return (
-    <>
-      {/* <Home /> */}
-      <VR />
-    </>
 
+    <Home />
 
   )
 }
