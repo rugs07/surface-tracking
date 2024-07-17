e7971f777d8a05c2b110e8701b25e7ae78447528
rugs07 <rugwed07@gmail.com>
2024-07-17 11:56:47 +0530

    testing on phone for both ring

--- src/source/Components/AR/FaceTrackingComponent.jsx
+++ src/source/Components/AR/FaceTrackingComponent.jsx
@@ -239,7 +239,7 @@ const HandTrackingComponent = () => {
             <>
               <Splat
                 src={ringUrl2}
-                scale= {[wristZoom,wristZoom,wristZoom]}
+                scale= {[0.1,0.1,0.1]}
                 rotation={[XRDelta, YRDelta, 0]}
               />
             </>
