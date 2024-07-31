3b1a0367cfebc0737d11014007361e6208f42c1c
rugs07 <rugwed07@gmail.com>
2024-07-31 23:37:56 +0530

    trying blipparAR

--- src/source/Components/AR/FaceTrackingComponent.jsx
+++ src/source/Components/AR/FaceTrackingComponent.jsx
@@ -61,7 +61,7 @@ const ARComponent = () => {
       </button>
       
       <a-scene
-      webar-scene="key: xxxxxxxx-1111-2222-3333-yyyyyyyyyyyy"
+      webar-scene="key: f59faf1a-5ffb-40de-9678-cf2957c4c1f0"
       vr-mode-ui="enabled: false"
       device-orientation-permission-ui="enabled: false"
       loading-screen="enabled: false">
